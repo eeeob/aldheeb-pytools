24094ce79bc96a9dd8346eb1c9c6f6b58ccb770e
Abdullah <aldheeb01@gmail.com>
2026-06-23 00:46:35 +0300

    feat: update version to 0.2.11 and add DelayedCallback class with async functionality

--- pytools/models/misc_models.py
+++ pytools/models/misc_models.py
@@ -1,18 +1,25 @@
-from typing import Generic, Tuple, Mapping, Optional, Any, Union, Dict
+from typing import Generic, Tuple, Mapping, Optional, Any, Union, Dict, Self
 from dataclasses import field, dataclass
 from pathlib import Path
 from threading import RLock
 from concurrent.futures import ThreadPoolExecutor
 
-from ..typings import MaybeAwaitable, _T, _KT, _VT
+from ..typings import (
+    MaybeAwaitable, Number, 
+    MaybeCoroutineCallable, 
+    _T, _KT, _VT
+)
 from ..enums import TriggerOn
 
 
 from ..validate_tools import is_exception
-from ..async_tools import maybe_awaitable, to_thread
+from ..async_tools import maybe_awaitable, to_thread, safe_wait_task
 from ..files_tools import load_json, save_json
 
 from .base import BaseDataClass
+
+
+import asyncio
 
 
 @dataclass(slots=True)
@@ -140,9 +147,83 @@ class JsonContainer(Generic[_KT, _VT], BaseDataClass):
             await self.async_save()
 
 
+@dataclass(slots=True)
+class DelayedCallback(Generic[_T]):
+    delay: Number
+    callback: MaybeAwaitable[..., _T]
+
+    args: Optional[Tuple[Any, ...]] = None
+    kw: Optional[Mapping[str, Any]] = None
+
+    on_finished: Optional[MaybeCoroutineCallable[[Self], Any]] = None
+    cancel_event: asyncio.Event = field(default_factory=asyncio.Event)
+
+    callback_started: bool = field(default=False, init=False)
+    _task: Optional[asyncio.Task[Optional[_T]]] = field(
+        default=None, 
+        init=False, 
+        repr=False, 
+    )
+
+    
+    async def wait(self) -> Optional[_T]:
+        try:
+            await asyncio.wait_for(self.cancel_event.wait(), timeout=self.delay)
+        except asyncio.TimeoutError:
+            self.callback_started = True
+
+            return await maybe_awaitable(
+                self.callback, 
+                *(self.args or ()), 
+                **(self.kw or {}) 
+            )
+
+        finally:
+            if self.on_finished is not None:
+                await maybe_awaitable(self.on_finished, self)
+
+    def start(self) -> asyncio.Task[Optional[_T]]:
+        if self._task is None:
+            self._task = asyncio.create_task(self.wait())
+
+        return self._task
+
+    async def cancel(self, force: bool = False) -> None:
+        task = self._task
+
+        if task is None:
+            return
+
+        self.cancel_event.set()
+
+        if not task.done():
+            if force or not self.callback_started:
+                task.cancel()
+
+            await safe_wait_task(task)
+
+        self._task = None
+
+    @property
+    def task(self) -> Optional[asyncio.Task[Optional[_T]]]:
+        return self._task
+
+    @property
+    def started(self) -> bool:
+        return self._task is not None
+
+    @property
+    def done(self) -> bool:
+        return self._task is not None and self._task.done()
+
+    @property
+    def cancelled(self) -> bool:
+        return self.cancel_event.is_set()
+
 
 __all__ = (
     "DeferredCall", 
     "JsonContainer", 
+    "DelayedCallback", 
 
 )
